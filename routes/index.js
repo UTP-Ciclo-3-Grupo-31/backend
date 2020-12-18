9958fd5928fa50b7ceba32ae3929ab51ba89f4a8
Sergio <sergi0vargas@users.noreply.github.com>
2020-12-18 12:21:41 -0500

    Cambiados 2 bobadas

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 //const router = require('express').Router(); // sobra de semana 3
 const routerx = require('express-promise-router');
-const apiRouterUser = require('./apis/users.js');//este sobra en semana 4
+//const apiRouterUser = require('./apis/users.js');//este sobra en semana 4
 const categoriaRouter = require('./categoria');
 const articuloRouter = require('./articulo');
 const usuarioRouter = require('./usuario');
@@ -9,7 +9,7 @@ const usuarioRouter = require('./usuario');
 const router = routerx();
 
 
-router.use('/auth', apiRouterUser); //sobra en semana 4
+//router.use('/auth', apiRouterUser); //sobra en semana 4
 
 router.use('/categoria', categoriaRouter);
 router.use('/articulo', articuloRouter);
